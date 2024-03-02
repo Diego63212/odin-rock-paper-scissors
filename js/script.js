ab36f1eca7a95b238a2dfbb7716e785915599c8f
Diego63212 <doom63212@gmail.com>
2024-03-01 23:31:09 -0300

    Add UI and reset function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
+const MAX_ROUNDS = 5;
+
+const container = document.querySelector('.container');
+const roundText = document.querySelector('#round-result')
+const scorePlayerText = document.querySelector('#round-won')
+const computerPlayerText = document.querySelector('#round-lost')
+const playerScore = document.querySelector('#score-player');
+const computerScore = document.querySelector('#score-computer');
+const neutralScore = document.querySelector('#score-tied')
+
 let computerChoice = getComputerChoice();
 let playerChoice = 'rock';
+
+let roundsWon = 0;
+let roundsLost = 0;
+let roundsTied = 0;
+
+let gameFinished = false;
+
+// Listen to click to get the player choice
+container.addEventListener('click', event => {
+    if (gameFinished) resetGame();
+
+    playGame(event.target.textContent)
+    event.stopPropagation();
+})
 
 // Returns the computer choice depending on a random number
 function getComputerChoice() {
@@ -23,39 +47,83 @@ function playRound(playerSelection, computerSelection) {
 
     // If both choices are the same declare a tie to end the round early
     if (playerSelection === computerSelection) {
-        console.log('Tie! Same choice');
+        roundText.textContent = 'Tie! Same choice';;
         return 'TIE';
     }
 
     // Player choice is Rock
     if (playerSelection === 'ROCK' && computerSelection === 'PAPER') {
-        console.log('You Lose! Paper beats Rock');
+        roundText.textContent = 'You Lose! Paper beats Rock';
         return 'LOSE';
     } else if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') {
-        console.log('You Win! Rock beats Scissors');
+        roundText.textContent = 'You Win! Rock beats Scissors';
         return 'WIN';
     }
 
     // Player choice is Paper
     if (playerSelection === 'PAPER' && computerSelection === 'ROCK') {
-        console.log('You Win! Paper beats Rock');
+        roundText.textContent = 'You Win! Paper beats Rock';
         return 'WIN';
     } else if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') {
-        console.log('You Lose! Scissors beats Paper');
+        roundText.textContent = 'You Lose! Scissors beats Paper';
         return 'LOSE';
     }
 
     // Player choice is Scissors
     if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
-        console.log('You Lose! Rock beats Scissors');
+        roundText.textContent = 'You Lose! Rock beats Scissors';
         return 'LOSE';
     } else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
-        console.log('You Win! Scissors beats Paper');
+        roundText.textContent = 'You Win! Scissors beats Paper';
         return 'WIN';
     }
 }
 
-function playGame() {
+function playGame(playerSelection) {
+    let roundResult = playRound(playerSelection, getComputerChoice());
+    
+    if (roundResult === 'WIN') {
+        roundsWon++;
+    } else if (roundResult === 'LOSE') {
+        roundsLost++;
+    } else if (roundResult === 'TIE') {
+        roundsTied++;
+    }
+
+    // Update scoring text
+    playerScore.textContent = roundsWon;
+    computerScore.textContent = roundsLost;
+    neutralScore.textContent = roundsTied;
+    
+    let totalRounds = roundsWon + roundsLost + roundsTied;
+
+    if (roundsWon === roundsLost && (totalRounds === MAX_ROUNDS)) {
+        gameFinished = true;
+        roundText.textContent = 'STALEMATE'
+        alert('STALEMATE!')
+        return 'STALEMATE';
+    } else if (roundsWon > roundsLost && (totalRounds === MAX_ROUNDS)) {
+        gameFinished = true;
+        roundText.textContent = 'WINNER!'
+        alert('You Win! Congratulations!')
+        return 'WINNER';
+    } else if (roundsWon < roundsLost && (totalRounds === MAX_ROUNDS)) {
+        gameFinished = true;
+        roundText.textContent = 'LOSER!'
+        alert('You Lose! Better luck next time!')
+        return 'LOSER';
+    }
+}
+
+function resetGame() {
+    roundText.textContent = 'Press a button!';
+    roundsWon = 0;
+    roundsLost = 0;
+    roundsTied = 0;
+    gameFinished = false;
+}
+
+/* function playGame() {
     let roundsWon = 0;
     let roundsLost = 0;
 
@@ -93,4 +161,4 @@ function playGame() {
         console.log('You Lose! Better luck next time!');
         return 'LOSER';
     }
-}
+} */
