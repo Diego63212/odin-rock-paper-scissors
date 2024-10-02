b00e8edfece157d1be5d3a1d7101d44123dd3f96
Diego63212 <doom63212@gmail.com>
2024-10-02 18:10:39 -0300

    Code refactor
    * Simplify if statements.
    * Reduce code duplication.
    * Improve game end logic (not final).
    * Add pointer cursor to buttons.
    * Remove unused variables.
    * Add and improve comments.
    * Remove debug code.
    * Reorder code.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,81 @@
 const MAX_ROUNDS = 6;
-
 const container = document.querySelector('.container');
 const roundText = document.querySelector('#round-result');
-
 const playerScoreText = document.querySelector('#score-player');
 const computerScoreText = document.querySelector('#score-computer');
 const tiedScoreText = document.querySelector('#score-tied');
-
-let computerChoice = getComputerChoice();
-let playerChoice = 'ROCK';
-
-let playerScore = 0;
-let computerScore = 0;
-let tiedScore = 0;
-
+let computerScore = playerScore = tiedScore = 0;
 let gameFinished = false;
 
+// Returns the computer choice depending on a random number
+function getComputerChoice() {
+    let randomChoice = Math.floor(Math.random() * 3 + 1);
+    if (randomChoice === 1) return 'Rock';
+    if (randomChoice === 2) return 'Paper';
+    if (randomChoice === 3) return 'Scissors';
+}
+// Play a single round and return the result
+function playRound(playerSelection, computerSelection) {
+    // If both choices are the same declare a tie to end the round early
+    if (playerSelection === computerSelection) return 'TIE';
+    // Player choice is Rock
+    if (playerSelection === 'Rock' && computerSelection === 'Scissors') return 'WIN';
+    if (playerSelection === 'Rock' && computerSelection === 'Paper') return 'LOSE';
+    // Player choice is Paper
+    if (playerSelection === 'Paper' && computerSelection === 'Rock') return 'WIN';
+    if (playerSelection === 'Paper' && computerSelection === 'Scissors') return 'LOSE';
+    // Player choice is Scissors
+    if (playerSelection === 'Scissors' && computerSelection === 'Paper') return 'WIN';
+    if (playerSelection === 'Scissors' && computerSelection === 'Rock') return 'LOSE';
+}
+// Main game function, handle game status
+function playGame(playerSelection) {
+    if (gameFinished) {
+        resetGame();
+        return;
+    }
+
+    const computerSelection = getComputerChoice();
+    let roundResult = playRound(playerSelection, computerSelection);
+    if (roundResult === 'WIN') {
+        playerScore++;
+        roundText.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+    if (roundResult === 'LOSE') {
+        computerScore++;
+        roundText.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+    if (roundResult === 'TIE') {
+        tiedScore++;
+        roundText.textContent = `Tie! Same choice (${playerSelection})`;
+    }
+    updateScore() // Input scores into UI text
+    
+    // Finish the game when maximum amount of rounds are played
+    let totalRounds = playerScore + computerScore + tiedScore;
+    if (totalRounds >= MAX_ROUNDS) {
+        let finishingText;
+        (playerScore === computerScore) ? finishingText = 'STALEMATE!' : undefined;
+        (playerScore > computerScore) ? finishingText = 'WINNER!' : undefined;
+        (playerScore < computerScore) ? finishingText = 'LOSER!' : undefined;
+        roundText.textContent = finishingText;
+        alert(finishingText); // Make it clear that the game ended
+        gameFinished = true;
+    }
+}
+// Update scoring text
+function updateScore() {
+    playerScoreText.textContent = playerScore;
+    computerScoreText.textContent = computerScore;
+    tiedScoreText.textContent = tiedScore;
+}
+// Reset the game status to initial
+function resetGame() {
+    roundText.textContent = 'Press a button!';
+    playerScore = computerScore = tiedScore = 0;
+    updateScore();
+    gameFinished = false;
+}
 // Listen to bubbling click to get the player choice
 container.addEventListener('click', event => {
     event.stopPropagation();
@@ -23,109 +83,3 @@ container.addEventListener('click', event => {
         playGame(event.target.textContent);
     }
 })
-
-// Returns the computer choice depending on a random number
-function getComputerChoice() {
-    let randomChoice = Math.floor(Math.random() * 3 + 1);
-    
-    if (randomChoice === 1) {
-        console.log('Computer: Rock');
-        return 'Rock';
-    } else if (randomChoice === 2) {
-        console.log('Computer: Paper');
-        return 'Paper';
-    } else if (randomChoice === 3) {
-        console.log('Computer: Scissors');
-        return 'Scissors';
-    }
-}
-
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toUpperCase();
-    computerSelection = computerSelection.toUpperCase();
-
-    // If both choices are the same declare a tie to end the round early
-    if (playerSelection === computerSelection) {
-        roundText.textContent = 'Tie! Same choice';;
-        return 'TIE';
-    }
-
-    // Player choice is Rock
-    if (playerSelection === 'ROCK' && computerSelection === 'PAPER') {
-        roundText.textContent = 'You Lose! Paper beats Rock';
-        return 'LOSE';
-    } else if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') {
-        roundText.textContent = 'You Win! Rock beats Scissors';
-        return 'WIN';
-    }
-
-    // Player choice is Paper
-    if (playerSelection === 'PAPER' && computerSelection === 'ROCK') {
-        roundText.textContent = 'You Win! Paper beats Rock';
-        return 'WIN';
-    } else if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') {
-        roundText.textContent = 'You Lose! Scissors beats Paper';
-        return 'LOSE';
-    }
-
-    // Player choice is Scissors
-    if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
-        roundText.textContent = 'You Lose! Rock beats Scissors';
-        return 'LOSE';
-    } else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
-        roundText.textContent = 'You Win! Scissors beats Paper';
-        return 'WIN';
-    }
-}
-
-function playGame(playerSelection) {
-    if (gameFinished) {
-        resetGame();
-        return
-    }
-    
-    let roundResult = playRound(playerSelection, getComputerChoice());
-
-    if (roundResult === 'WIN') {
-        playerScore++;
-    } else if (roundResult === 'LOSE') {
-        computerScore++;
-    } else if (roundResult === 'TIE') {
-        tiedScore++;
-    }
-
-    // Input scores into UI text
-    updateScore()
-    
-    let totalRounds = playerScore + computerScore + tiedScore;
-
-    if (playerScore === computerScore && (totalRounds === MAX_ROUNDS)) {
-        gameFinished = true;
-        roundText.textContent = 'STALEMATE!'
-        alert('Stalemate!')
-    } else if (playerScore > computerScore && (totalRounds === MAX_ROUNDS)) {
-        gameFinished = true;
-        roundText.textContent = 'WINNER!'
-        alert('You Win! Congratulations!')
-    } else if (playerScore < computerScore && (totalRounds === MAX_ROUNDS)) {
-        gameFinished = true;
-        roundText.textContent = 'LOSER!'
-        alert('You Lose! Better luck next time!')
-    }
-}
-
-function updateScore() {
-    // Update scoring text
-    playerScoreText.textContent = playerScore;
-    computerScoreText.textContent = computerScore;
-    tiedScoreText.textContent = tiedScore;
-}
-// Allows the game to be played again after ending
-function resetGame() {
-    roundText.textContent = 'Press a button!';
-    playerScore = 0;
-    computerScore = 0;
-    tiedScore = 0;
-    updateScore()
-    gameFinished = false;
-}
